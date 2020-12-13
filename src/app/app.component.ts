21ecde4b36b32900cd89dad5442c833c9b74ec08
Rituparna Warwatkar <rwarwatkar@gmail.com>
2020-12-13 07:42:19 +0530

    Word Gen Project Complete ng build --prod --output-path docs --base-href /<project_name>/

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import arrayOfWords from './../utils/words';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'word-generator';
+  title = 'Word Generator';
+  words = '';
+  limit = 10;
+
+  handleSlideChange(newLimit: number) {
+    this.limit = newLimit;
+  }
+
+  generate() {
+    this.words = arrayOfWords.slice(0, this.limit).join(' ');
+  }
 }
